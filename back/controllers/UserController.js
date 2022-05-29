31cc8d057f4c0ed8ebe92eae9a5993158bd1744e
Kristeauf <kristeauf@gmail.com>
2022-05-29 05:34:31 +0200

    signup et login user ok

--- back/controllers/UserController.js
+++ back/controllers/UserController.js
@@ -3,83 +3,65 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const db = require('../dbconfig')
 
-
-
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
+    
         .then(hash => {
-           
+
             const userName = req.body.userName;
             const email = req.body.email;
             const password = hash;
-            // const query = (`INSERT INTO user (userName, email, password) VALUES('${userName}','${email}','${password}')`)
-
-            const sql ="INSERT INTO User (userName, email, password) VALUES (?, ?, ?);"
+            const sql = "INSERT INTO User (userName, email, password) VALUES (?, ?, ?);"
             const sqlParams = [userName, email, password];
-           db.query(sql, sqlParams, (error, results, fields) => {
+
+            db.query(sql, sqlParams, (error, results, fields) => {
                 if (error) {
-                  if (error.errno === 1062) {
-                    res.status(403).json({ error: "email ou nom d'utilisateur deja utilisé" });
-                  } else {
-                    res.status(500).json({ error: error.sqlMessage });
-                  }
+                    if (error.errno === 1062) {
+                        res.status(403).json({ error: "email ou nom d'utilisateur deja utilisé" });
+                    } else {
+                        res.status(500).json({ error: error.sqlMessage });
+                    }
                 } else {
-                  res.status(201).json({ message: "Utilisateur créé" });
+                    res.status(201).json({ message: "Utilisateur créé" });
                 }
-                });
-               
-            
+            });
         });
 
 }
-//  const sql ="INSERT INTO Users (name, email, password, isadmin) VALUES (?, ?, ?);";
-// const sqlParams = [userName, email, password];
-
-// connection.execute(sql, sqlParams, (error, results, fields) => {
-// if (error) {
-//   if (error.errno === 1062) {
-//     res.status(403).json({ error: "L'email est déjà utilisé !" });
-//   } else {
-//     res.status(500).json({ error: error.sqlMessage });
-//   }
-// } else {
-//   res.status(201).json({ message: "Utilisateur créé" });
-// }
-// });
-// connection.end();
-// const dbUser = db.query(`INSERT INTO user (userName, email, password) VALUES('${userName}','${email}','${password}')`)
-
-//                 .then(() =>
-
-//                     res.status(201).json({ message: 'Utilisateur créé !' }))
-
-//                 .catch(error => res.status(400).json({ error }));
-//         })
-//         .catch(error => res.status(500).json({ error }));
 
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-        .then(user => {
 
-            if (!user) {
-                return res.status(403).json({ error: 'utilisateur introuvable' })
-            }
-            bcrypt.compare(req.body.password, user.password)
+    const researchedEmail = req.body.email;
+    const sql = "SELECT id,userName, email, password FROM user WHERE email= ?";
+    const sqlParams = [researchedEmail];
+
+    db.query(sql, sqlParams, (error, results, fields) => {
+
+        if (error) {
+            res.status(500).json({ error: error.sqlMessage })
+        }
+        else if (results.length == 0 || results[0].userName != req.body.userName) {
+            res.status(401).json({ error: "Nom d'utilisateur ou email incorrect!" });
+        }
+        else {
+            const hash = results[0].password;
+            bcrypt.compare(req.body.password, hash)
                 .then(valid => {
+
                     if (!valid) {
                         return res.status(401).json({ error: 'mot de passe invalide' })
                     }
                     res.status(200).json({
-                        userId: user._id,
+                        id: results[0].id,
                         token: jwt.sign(
-                            { userId: user._id },
+                            { id: results[0].id },
                             process.env.TOKEN,
                             { expiresIn: '24h' }
                         )
                     })
-                })
-                .catch(error => res.status(500).json({ error }))
-        })
-        .catch(error => res.status(500).json({ error }))
+                }).catch(error => res.status(500).json({ error }))
+        }
+    })
 }
+
 
